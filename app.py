6e149673766018d7b1a0198c9cdbf7b765b7ebd6
Lizelle <lizellesm@gmail.com>
2025-03-12 17:16:20 +0200

    afr

--- app.py
+++ app.py
@@ -10,75 +10,6 @@ app = Flask(__name__)
 
 # DB--------------------------------------------------------------------------------------------
 
-# Database Configuration
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'  # Change this to your database URL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Zakkie2910#@localhost/user'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Optional, to suppress warnings
-#MySQL db
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = 'mysecret'
-
-# Initialize Database
-db = SQLAlchemy(app)
-csrf = CSRFProtect(app)
-
-# Database Model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"<User {self.name}>"
-
-def create_tables():
-    with app.app_context():
-        db.create_all()
-
-@app.route('/test_db')
-def test_db():
-    try:
-        users = User.query.all()
-        return f"Connected! Found {len(users)} users."
-    except Exception as e:
-        return str(e)
-
-
-
-class RegistrationForm(FlaskForm):
-    name = StringField('Naam', validators=[DataRequired()])
-    email = StringField('Epos', validators=[DataRequired(), Email()])
-    submit = SubmitField('Registreer')        
-
-@app.route('/register', methods=['GET', 'POST'])
-def register_user():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        email = form.email.data
-
-        if User.query.filter_by(email=email).first():
-            flash("Die epos adres is reeds geregistreer!", "warning")
-            return redirect(url_for('register_user'))
-
-        new_user = User(name=name, email=email)
-        db.session.add(new_user)
-        db.session.commit()
-
-        flash("Registrasie suksesvol", "success")
-        return redirect(url_for('homegr4'))
-
-    return render_template('register.html', form=form)
-
-@app.route('/user')
-def user():
-    our_users = User.query.all()  # Fetch all users from the database
-    return render_template('user.html', our_users=our_users)
-
-
-
 
 
 
@@ -91,10 +22,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-# Create Database
-def create_db():
-    with app.app_context():
-        db.create_all()
 
 
 
